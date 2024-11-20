06c75633d5f941b142cec870bf82185fc012adfd
Sacha TOPALOFF <sacha.topaloff@epitech.eu>
2024-11-20 19:42:57 +0900

    feat: add AND cpu instruction
    resolves #37

--- src/CPU.cpp
+++ src/CPU.cpp
@@ -225,6 +225,15 @@ void CPU::addWordUnsigned(const Instruction &instruction)
     setReg(instruction.r.rd, tmp);
 }
 
+void CPU::andWord(const Instruction &instruction)
+{
+    uint32_t left = getReg(instruction.r.rs);
+    uint32_t right = getReg(instruction.r.rt);
+    uint32_t res = left & right;
+
+    setReg(instruction.r.rd, res);
+}
+
 void CPU::illegalInstruction(const Instruction &instruction)
 {
     fprintf(stderr, "Illegal instruction: ");
