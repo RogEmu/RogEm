9c09ae0282ccd7f3ed6e07182778855ceaa30b28
Sacha TOPALOFF <sacha.topaloff@epitech.eu>
2024-11-25 11:14:54 +0900

    tests: add SRLV tests
    resolves #69

--- tests/CPU_shift_tests.cpp
+++ tests/CPU_shift_tests.cpp
@@ -131,3 +131,60 @@ TEST(CpuTest, SRL_Large_Shift)
 
     EXPECT_EQ(cpu.m_registers[i.r.rd], value >> (shiftAmount & 0x1F));
 }
+
+TEST(CpuTest, SRLV_1)
+{
+    BIOS bios;
+    Bus bus(bios);
+    CPU cpu(bus);
+    Instruction i;
+
+    uint32_t value = 0x9F0DE23B;
+    uint8_t shiftAmount = 13; // Bigger shift value should be truncated
+    loadImmediate(cpu, 8, value); // Value that will be shifted in register $8
+    loadImmediate(cpu, 9, shiftAmount); // Shift amount
+    i.r.rs = 9;
+    i.r.rt = 8;
+    i.r.rd = 8;
+    cpu.shiftRightLogicalVariable(i);
+
+    EXPECT_EQ(cpu.m_registers[i.r.rd], value >> (shiftAmount & 0x1F));
+}
+
+TEST(CpuTest, SRLV_Large_Shift)
+{
+    BIOS bios;
+    Bus bus(bios);
+    CPU cpu(bus);
+    Instruction i;
+
+    uint32_t value = 0x9F0DE23B;
+    uint32_t shiftAmount = 78127942; // Bigger shift value should be truncated
+    loadImmediate(cpu, 8, value); // Value that will be shifted in register $8
+    loadImmediate(cpu, 9, shiftAmount); // Shift amount
+    i.r.rs = 9;
+    i.r.rt = 8;
+    i.r.rd = 8;
+    cpu.shiftRightLogicalVariable(i);
+
+    EXPECT_EQ(cpu.m_registers[i.r.rd], value >> (shiftAmount & 0x1F));
+}
+
+TEST(CpuTest, SRLV_No_Shift)
+{
+    BIOS bios;
+    Bus bus(bios);
+    CPU cpu(bus);
+    Instruction i;
+
+    uint32_t value = 0x9F0DE23B;
+    uint32_t shiftAmount = 0; // Bigger shift value should be truncated
+    loadImmediate(cpu, 8, value); // Value that will be shifted in register $8
+    loadImmediate(cpu, 9, shiftAmount); // Shift amount
+    i.r.rs = 9;
+    i.r.rt = 8;
+    i.r.rd = 8;
+    cpu.shiftRightLogicalVariable(i);
+
+    EXPECT_EQ(cpu.m_registers[i.r.rd], value);
+}
