64a33699e577bc8fcf131f882e416083722d682f
tovyREN <tovy.ren@epitech.eu>
2025-02-10 23:44:59 +0900

    tests: add JR test

--- tests/CPU_jump_tests.cpp
+++ tests/CPU_jump_tests.cpp
@@ -29,3 +29,18 @@ TEST(CpuTest, JUMP_AND_LINK)
     EXPECT_EQ(cpu.m_pc, address);
     EXPECT_EQ(cpu.m_registers[31], 0x0);
 }
+
+TEST(CpuTest, JUMP_REGISTER)
+{
+    BIOS bios;
+    Bus bus(bios);
+    CPU cpu(bus);
+    Instruction i;
+
+    uint32_t address = 0x12345678;
+    loadImmediate(cpu, 8, address);
+    i.r.rs = 8;
+    cpu.jumpRegister(i);
+
+    EXPECT_EQ(cpu.m_pc, address);
+}
