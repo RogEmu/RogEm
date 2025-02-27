86ab6d812d9c839c4a40190717071561c764592a
tovyREN <tovy.ren@epitech.eu>
2025-02-27 17:10:23 +0900

    test: add test for jalr

--- tests/CPU_jump_tests.cpp
+++ tests/CPU_jump_tests.cpp
@@ -44,3 +44,19 @@ TEST(CpuTest, JUMP_REGISTER)
 
     EXPECT_EQ(cpu.m_pc, address);
 }
+
+TEST(CpuTest, JUMP_AND_LINK_REGISTER)
+{
+    BIOS bios;
+    Bus bus(bios);
+    CPU cpu(bus);
+    Instruction i;
+
+    uint32_t address = 0x12345678;
+    loadImmediate(cpu, 8, address);
+    i.r.rs = 8;
+    cpu.jumpAndLinkRegister(i);
+
+    EXPECT_EQ(cpu.m_pc, address);
+    EXPECT_EQ(cpu.m_registers[31], 0x0);
+}
