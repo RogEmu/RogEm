dd719bd7cf69ba23874e6e4fa7c72a51c630fb70
Sacha TOPALOFF <sacha.topaloff@epitech.eu>
2025-03-12 23:40:52 +0900

    feat: implement mtc0 instruction

--- src/CPU.h
+++ src/CPU.h
@@ -113,6 +113,10 @@ struct CPU
     void executeCop0(const Instruction &instruction);
     void mtc0(const Instruction &instruction);
 
+    //COP0 Instructions
+    void executeCop0(const Instruction &instruction);
+    void mtc0(const Instruction &instruction);
+
     void illegalInstruction(const Instruction &instruction);
     void specialInstruction(const Instruction &instruction);
     void branchOnConditionZero(const Instruction &instruction);
