742e633a8c4c02a1340ecd67982d1b950591487d
Sacha TOPALOFF <sacha.topaloff@epitech.eu>
2025-03-17 00:07:42 +0900

    feat: implement a simple pausing system for the debugger

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,7 @@ void init_ncurses()
     noecho();              // Don't echo user input
     keypad(stdscr, TRUE);  // Enable arrow keys
     curs_set(0);           // Hide the cursor
+    nodelay(stdscr, true);
     refresh();
     start_color();
 
@@ -59,15 +60,31 @@ int main(int ac, char **av)
     insWin.setPc(&cpu.m_pc);
     insWin.setBus(&bus);
 
+    int ch = 0;
+    bool pause = false;
+
     while (true)
     {
+        ch = getch();
+
+        fprintf(stderr, "%c", ch);
+
+        if (ch == 'p')
+        {
+            pause = !pause;
+        }
+        ch = 0;
+
+        if (pause)
+            continue;
+
         cpu.step();
         insWin.addInstruction(cpu.m_pc, Instruction{bus.loadWord(cpu.m_pc)});
         insWin.draw();
         regWin.draw();
         // draw_registers(cpu.m_registers, cpu.m_hi, cpu.m_lo, cpu.m_pc);
         // draw_instructions(cpu.m_pc, {});
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
     cleanup_ncurses();
     return 0;
