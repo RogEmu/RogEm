9c39f1d04155e11eff5f34f0ac791c474ec91023
tovyREN <tovy.ren@epitech.eu>
2025-03-25 21:03:27 +0900

    fix: docking a window no longer make the program crash resolve#101

--- src/Debugger/Debugger.cpp
+++ src/Debugger/Debugger.cpp
@@ -9,6 +9,7 @@
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 #include <fmt/format.h>
+#
 
 #include "Debugger.hpp"
 #include "CPU.h"
@@ -30,44 +31,41 @@ void Debugger::registerTable()
     auto tableFlags = ImGuiTableFlags_Borders
                     | ImGuiTableFlags_RowBg;
     ImGui::Begin("Registers");
-    ImGui::BeginTable("Registers", 2, tableFlags);
-    ImGui::TableSetupColumn("Register");
-    ImGui::TableSetupColumn("Value");
-    ImGui::TableHeadersRow();
-    for (uint8_t i = 0; i < NB_GPR; i++) {
-        ImGui::TableNextRow();
+    if(ImGui::BeginTable("Registers", 2, tableFlags))
+    {
+        ImGui::TableSetupColumn("Register");
+        ImGui::TableSetupColumn("Value");
+        ImGui::TableHeadersRow();
+        for (int i = 0; i < NB_GPR; i++) {
+            ImGui::TableNextRow();
+            ImGui::TableNextColumn();
+            ImGui::Text("%s", Disassembler::getRegisterName(i).c_str());
+            ImGui::TableNextColumn();
+            ImGui::Text("%08X", m_cpu->m_registers[i]);
+        }
+        const char* extraRegisters[] = {"PC", "HI", "LO"};
+        uint32_t extraValues[] = {m_cpu->m_pc, m_cpu->m_hi, m_cpu->m_lo};
+
+        for (int i = 0; i < 3; i++) {
+            ImGui::TableNextRow();
+            ImGui::TableNextColumn();
+            ImGui::Text("%s", extraRegisters[i]);
+            ImGui::TableNextColumn();
+            ImGui::Text("%08X", extraValues[i]);
+        }
+
+        //COP0 Registers
         ImGui::TableNextColumn();
-        ImGui::Text("%s", Disassembler::getRegisterName(i).c_str());
+        ImGui::Text("COP0-SR");
         ImGui::TableNextColumn();
-        ImGui::Text("%08X", m_cpu->m_registers[i]);
+        ImGui::Text("%08X", m_cpu->m_cop0Reg[12]);
+        ImGui::TableNextColumn();
+        ImGui::Text("COP0-CAUSE");
+        ImGui::TableNextColumn();
+        ImGui::Text("%08X", m_cpu->m_cop0Reg[13]);
+
+        ImGui::EndTable();
     }
-    ImGui::TableNextRow();
-    ImGui::TableNextColumn();
-    ImGui::Text("PC");
-    ImGui::TableNextColumn();
-    ImGui::Text("%08X", m_cpu->m_pc);
-    ImGui::TableNextRow();
-    ImGui::TableNextColumn();
-    ImGui::Text("HI");
-    ImGui::TableNextColumn();
-    ImGui::Text("%08X", m_cpu->m_hi);
-    ImGui::TableNextRow();
-    ImGui::TableNextColumn();
-    ImGui::Text("LO");
-    ImGui::TableNextColumn();
-    ImGui::Text("%08X", m_cpu->m_lo);
-
-    //COP0 Registers
-    ImGui::TableNextColumn();
-    ImGui::Text("COP0-SR");
-    ImGui::TableNextColumn();
-    ImGui::Text("%08X", m_cpu->m_cop0Reg[12]);
-    ImGui::TableNextColumn();
-    ImGui::Text("COP0-CAUSE");
-    ImGui::TableNextColumn();
-    ImGui::Text("%08X", m_cpu->m_cop0Reg[13]);
-
-    ImGui::EndTable();
     ImGui::End();
 }
 
@@ -140,29 +138,30 @@ void Debugger::InstructionTable()
     auto tableFlags = ImGuiTableFlags_RowBg
                     | ImGuiTableFlags_Borders;
 
-    ImGui::BeginTable("Instructions", 1, tableFlags);
-    ImGui::TableSetupColumn("Instructions");
-    ImGui::TableHeadersRow();
+    if (ImGui::BeginTable("Instructions", 1, tableFlags)) {
+        ImGui::TableSetupColumn("Instructions");
+        ImGui::TableHeadersRow();
 
-    int nbInstructions = 32;
-    int half = nbInstructions / 2;
+        int nbInstructions = 32;
+        int half = nbInstructions / 2;
 
-    for (int i = -half; i < half; i++) {
-        ImGui::TableNextColumn();
-        uint32_t pc = m_cpu->m_pc + i * 4;
-        Instruction instr = {.raw = m_cpu->m_bus->loadWord(pc)};
-        auto str = Disassembler::disassemble(pc, instr);
-        if (pc == m_cpu->m_pc)
-        {
-            ImGui::PushStyleColor(ImGuiCol_Text, IM_COL32(255, 200, 0, 255));
-            ImGui::Text("%s", str.c_str());
-            ImGui::PopStyleColor();
-        } else
-        {
-            ImGui::Text("%s", str.c_str());
+        for (int i = -half; i < half; i++) {
+            ImGui::TableNextColumn();
+            uint32_t pc = m_cpu->m_pc + i * 4;
+            Instruction instr = {.raw = m_cpu->m_bus->loadWord(pc)};
+            auto str = Disassembler::disassemble(pc, instr);
+            if (pc == m_cpu->m_pc)
+            {
+                ImGui::PushStyleColor(ImGuiCol_Text, IM_COL32(255, 200, 0, 255));
+                ImGui::Text("%s", str.c_str());
+                ImGui::PopStyleColor();
+            } else
+            {
+                ImGui::Text("%s", str.c_str());
+            }
         }
+        ImGui::EndTable();
     }
-    ImGui::EndTable();
     ImGui::End();
 }
 
