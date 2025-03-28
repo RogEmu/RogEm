5ff28527c7af5ad5ddb4446695a84cdf2c3463dc
Sacha TOPALOFF <sacha.topaloff@epitech.eu>
2025-03-28 21:43:42 +0900

    feat: add memory imgui editor

--- src/Debugger/MemoryWindow.cpp
+++ src/Debugger/MemoryWindow.cpp
@@ -1,6 +1,7 @@
 #include "MemoryWindow.hpp"
 #include "imgui.h"
 #include <fmt/format.h>
+#include "imgui/imgui_memory_editor.h"
 
 #include "Debugger.hpp"
 
@@ -15,37 +16,8 @@ MemoryWindow::~MemoryWindow()
 
 void MemoryWindow::update()
 {
-    ImGui::Begin("Memory");
+    static MemoryEditor mem_edit;
 
-    auto tableFlags = ImGuiTableFlags_Borders
-                    | ImGuiTableFlags_RowBg;
-
-    if (ImGui::BeginTable("Memory", 18, tableFlags)) {
-        ImGui::TableSetupColumn("Address");
-        for (int i = 0; i < 16; i++) {
-            ImGui::TableSetupColumn(fmt::format("{:X}", i).c_str());
-        }
-        ImGui::TableSetupColumn("ASCII");
-        ImGui::TableHeadersRow();
-
-        for (int i = 0; i < 256; i += 16) {
-            ImGui::TableNextRow();
-            ImGui::TableNextColumn();
-            ImGui::Text("%08X", i);
-
-            std::string asciiStr;
-            for (int j = 0; j < 16; j++) {
-                uint8_t byte = m_debugger->readByte(i + j);
-                ImGui::TableNextColumn();
-                ImGui::Text("%02X", byte);
-
-                char c = byte > 126 || byte < 32 ? '.' : byte;
-                asciiStr += c;
-            }
-            ImGui::TableNextColumn();
-            ImGui::Text("%s", asciiStr.c_str());
-        }
-        ImGui::EndTable();
-    }
-    ImGui::End();
+    const char *data = "Hello guys! This is a test to see if the memory editor is working!";
+    mem_edit.DrawWindow("Memory Editor", (void*)data, 67, 0xFFFF);
 }
