f0c174df9136743029abfaf7fc8ac76be22d4df1
tovyREN <tovy.ren@epitech.eu>
2025-04-07 11:33:10 +0900

    test: add tests for branch on not equal resolve#136

--- tests/CPU_branch_tests.cpp
+++ tests/CPU_branch_tests.cpp
@@ -43,5 +43,77 @@ TEST(CpuTest, BEQ_2)
     i.i.immediate = imm;
     cpu.branchOnEqual(i);
 
-    EXPECT_EQ(cpu.pc, RESET_VECTOR + 4);  // ✅ Correct expected behavior
+    EXPECT_EQ(cpu.pc, RESET_VECTOR + 4);
+}
+
+TEST(CpuTest, BNE_BranchWhenNotEqual) {
+    auto bios = BIOS();
+    auto bus = Bus(&bios, nullptr);
+    CPU cpu(&bus);
+    Instruction i;
+
+    cpu.pc = RESET_VECTOR;
+    cpu.gpr[1] = 0x1234;
+    cpu.gpr[2] = 0x5678;
+
+    i.i.rs = 1;
+    i.i.rt = 2;
+    i.i.immediate = 0x0004;
+
+    cpu.branchOnNotEqual(i);
+    EXPECT_EQ(cpu.m_branchSlotAddr, RESET_VECTOR + 4 + ((int16_t)i.i.immediate << 2));
+}
+
+TEST(CpuTest, BNE_NoBranchWhenEqual) {
+    auto bios = BIOS();
+    auto bus = Bus(&bios, nullptr);
+    CPU cpu(&bus);
+    Instruction i;
+
+    cpu.pc = RESET_VECTOR;
+    cpu.gpr[3] = 0xDEAD;
+    cpu.gpr[4] = 0xDEAD;
+
+    i.i.rs = 3;
+    i.i.rt = 4;
+    i.i.immediate = 0x0004;
+
+    cpu.branchOnNotEqual(i);
+    EXPECT_EQ(cpu.pc + 4, RESET_VECTOR + 4);  // No branch
+}
+
+TEST(CpuTest, BNE_NoBranch_ZeroRegistersEqual) {
+    auto bios = BIOS();
+    auto bus = Bus(&bios, nullptr);
+    CPU cpu(&bus);
+    Instruction i;
+
+    cpu.pc = RESET_VECTOR;
+    cpu.gpr[0] = 0;  // $zero
+    cpu.gpr[1] = 0;
+
+    i.i.rs = 0;
+    i.i.rt = 1;
+    i.i.immediate = 2;
+
+    cpu.branchOnNotEqual(i);
+    EXPECT_EQ(cpu.pc + 4, RESET_VECTOR + 4);  // No branch
+}
+
+TEST(CpuTest, BNE_MaxPositiveOffset) {
+    auto bios = BIOS();
+    auto bus = Bus(&bios, nullptr);
+    CPU cpu(&bus);
+    Instruction i;
+
+    cpu.pc = RESET_VECTOR;
+    cpu.gpr[2] = 100;
+    cpu.gpr[3] = 200;
+
+    i.i.rs = 2;
+    i.i.rt = 3;
+    i.i.immediate = 0x7FFF;
+
+    cpu.branchOnNotEqual(i);
+    EXPECT_EQ(cpu.m_branchSlotAddr, RESET_VECTOR + 4 + (0x7FFF << 2));
 }
