7d4c9ca0c6825c18e489032dbc11d7f417d7e6a7
Sacha TOPALOFF <sacha.topaloff@epitech.eu>
2026-01-21 11:15:26 +0100

    feat: reset devices from system reset method

--- src/Core/RAM.cpp
+++ src/Core/RAM.cpp
@@ -12,6 +12,11 @@ RAM::RAM(Bus *bus) :
     m_memoryRange.length = MemoryMap::RAM_RANGE.length;
 }
 
+void RAM::reset()
+{
+    std::memset(m_data.data(), 0, m_data.size());
+}
+
 void RAM::loadExecutable(uint32_t baseAddr, const std::vector<uint8_t> &code)
 {
     uint32_t mappedBase = MemoryMap::mapAddress(baseAddr);
